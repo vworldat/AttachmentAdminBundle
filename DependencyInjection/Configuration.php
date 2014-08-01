496e4ec30e99a53cd2f78a4a2c165a1dbbf8d373
David Herrmann <david@vworld.at>
2014-08-01 18:45:41 +0200

    First working release

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -18,6 +18,8 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('c33s_attachment_admin');
+
         return $treeBuilder;
     }
 }
